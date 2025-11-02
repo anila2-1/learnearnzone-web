ac6158376149bc951522fa657094cdf16e7f0ecf
anila2-1 <w1techy8@gmail.com>
2025-11-02 21:29:31 +0500

    fix

---
+++ next-sitemap.config.cjs
@@ -0,0 +1,18 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://quiz-learn-web.vercel.app',
+  generateRobotsTxt: true,
+  // optional
+  // changefreq: 'daily',
+  // priority: 0.7,
+  // sitemapSize: 5000,
+  // exclude: ['/protected-page', '/another-page'],
+  // robotsTxtOptions: {
+  //   policies: [
+  //     {
+  //       userAgent: '*',
+  //       allow: '/',
+  //     },
+  //   ],
+  // },
+}
